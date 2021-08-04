1b0ceab6b14ab47a6b293617c2e342bd2ca66cb3
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-08-04 17:15:18 +0300

    TruncFinansv2 - 3

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Dropdown } from "react-bootstrap";
 
-const Footer= ()=> {
-    return (
-        <footer className="Footer">
-            <ul className="footerAPILinks">
-                <li>
-                    <a href="https://truncgil.com.tr/">Trunçgil Teknoloji </a> Copyright © 2021
-                </li>
-                <li>
-                    API: <a href="https://finans.truncgil.com/today.json">https://finans.truncgil.com/today.json</a>
-                </li>
-                <li>
-                    API v2: <a href="https://finans.truncgil.com/v2/today.json">https://finans.truncgil.com/v2/today.json</a>
-                </li>
-                <li>
-                    API v3: <a href="https://finans.truncgil.com/v3/today.json">https://finans.truncgil.com/v3/today.json</a>
-                </li>
-            </ul>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="Footer pt-5 pb-5">
+      <ul className="footerTruncgil">
+        <li>
+          <a href="https://truncgil.com.tr/">Trunçgil Teknoloji </a>
+        </li>
+        <li className="footerCopyright">
+          Copyright © 2021 Her Hakkı Saklıdır.
+        </li>
+      </ul>
+      <ul className="footerContact">
+          <li className="mb-2"><span className="footerHelpSpan">Destek</span></li>
+          <li><a href="https://truncgil.com.tr/#neler-yapariz">Hakkımızda</a></li>
+          <li><a href="https://truncgil.com.tr/#iletisim">İletişim</a></li>
+          <Dropdown drop="up">
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            API
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="https://finans.truncgil.com/today.json">API</Dropdown.Item>
+            <Dropdown.Item href="https://finans.truncgil.com/v2/today.json">API v2</Dropdown.Item>
+            <Dropdown.Item href="https://finans.truncgil.com/v3/today.json">API v3</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </ul>
+    </footer>
+  );
+};
 
 export default Footer;
